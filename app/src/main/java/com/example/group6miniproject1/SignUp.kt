eef731048beaae4f45377953783c2f1a7ea48c5f
sarahwonago <sarahshaaleh@gmail.com>
2023-11-22 18:21:16 +0300

    added signup activity

---
+++ app/src/main/java/com/example/group6miniproject1/SignUp.kt
@@ -0,0 +1,11 @@
+package com.example.group6miniproject1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
